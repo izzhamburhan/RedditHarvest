c1ce948bbb6030c5125ea2add17a51dcba0e3680
Muhammad Ariff Izzham Bin Burhanuddin <ARIFF.BURHANUDDIN@TMRND.COM.MY>
2025-01-02 10:34:36 +0800

    feat : adjusting docker compose | adjusting dag/etls/pipelines/utils

---
+++ pipelines/reddit_pipeline.py
@@ -0,0 +1,12 @@
+from etls.reddit_etl import connect_reddit, extract_posts
+from utils.constants import CLIENT_ID, SECRET
+
+def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None):
+    #connecting to reddit instance
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+
+    #extraction
+    posts = extract_posts(instance, subreddit, time_filter, limit)
+    #transformation
+
+    #loading to csv
---
+++ utils/constants.py
@@ -0,0 +1,20 @@
+import configparser
+import os 
+
+parser = configparser.ConfigParser()
+parser.read(os.path.join(os.path.dirname(__file__), '../config/config.conf'))
+
+SECRET = parser.get('api_keys', 'reddit_secret_key')
+CLIENT_ID = parser.get('api_keys', 'reddit_secret_key')
+
+DATABASE_HOST = parser.get('database', 'database_host')
+DATABASE_NAME = parser.get('database', 'database_name')
+DATABASE_PORT = parser.get('database', 'database_port')
+DATABASE_USER = parser.get('database', 'database_username')
+DATABASE_PASSWORD = parser.get('database', 'database_password')
+
+INPUT_PATH = parser.get('file_paths', 'input_path')
+OUTPUT_PATH = parser.get('file_paths', 'output_path')
+
+
+
